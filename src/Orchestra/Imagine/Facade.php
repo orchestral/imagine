cdce46464630311a3bff161312eebf577f88e301
crynobone <crynobone@gmail.com>
2013-09-18 13:44:47 +0800

    Add basic manager and facade.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Imagine/Facade.php
@@ -0,0 +1,11 @@
+<?php namespace Orchestra\Imagine;
+
+class Facade extends \Illuminate\Support\Facades\Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'orchestra.imagine'; }
+}
