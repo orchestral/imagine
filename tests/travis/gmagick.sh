0e76ea207dc64a51a578128320940c54ae0b5d4f
crynobone <crynobone@gmail.com>
2014-09-03 20:19:36 +0800

    fixes cd to wrong dir.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/travis/gmagick.sh
+++ tests/travis/gmagick.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y graphicsmagick libgraphicsmagick1-dev;
 wget http://pecl.php.net/get/gmagick-1.1.7RC2.tgz;
 tar -xzf gmagick-1.1.7RC2.tgz;
-cd gmagick-1.1.7RC3;
+cd gmagick-1.1.7RC2;
 phpize;
 ./configure --with-gmagick=/usr/local;
 make -j;
