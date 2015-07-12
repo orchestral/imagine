3391c690739eb0bd939343e3a9f48c528a22a480
crynobone <crynobone@gmail.com>
2015-07-13 07:14:39 +0800

    Use releases folder.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/travis/imagick.sh
+++ tests/travis/imagick.sh
@@ -1,4 +1,4 @@
-wget http://www.imagemagick.org/download/ImageMagick-6.8.9-10.tar.xz;
+wget http://www.imagemagick.org/download/releases/ImageMagick-6.8.9-10.tar.xz;
 tar -xf ImageMagick-6.8.9-10.tar.xz;
 cd ImageMagick-6.8.9-10;
 ./configure --prefix=/opt/imagemagick;
