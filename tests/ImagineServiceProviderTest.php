46df4d9458597cf058bcea0b43027612012299b7
crynobone <crynobone@gmail.com>
2017-09-02 16:07:18 +0800

    Update project setup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/ImagineServiceProviderTest.php
+++ tests/ImagineServiceProviderTest.php
@@ -1,10 +1,11 @@
 <?php namespace Orchestra\Imagine\TestCase;
 
 use Mockery as m;
+use PHPUnit\Framework\TestCase;
 use Illuminate\Container\Container;
 use Orchestra\Imagine\ImagineServiceProvider;
 
-class ImagineServiceProviderTest extends \PHPUnit_Framework_TestCase
+class ImagineServiceProviderTest extends TestCase
 {
     /**
      * Teardown the test environment.
