fa3f7a9c74a30cef84bd2de754f93d2b58d9e4cc
crynobone <crynobone@gmail.com>
2017-09-03 11:09:17 +0800

    Restructure travis.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/travis/gmagick.sh
+++
@@ -1,6 +0,0 @@
-wget http://78.108.103.11/MIRROR/ftp/GraphicsMagick/1.3/GraphicsMagick-1.3.23.tar.xz;
-tar -xf GraphicsMagick-1.3.23.tar.xz;
-cd GraphicsMagick-1.3.23;
-./configure --prefix=/opt/gmagick --enable-shared --with-lcms2;
-make -j;
-sudo make install;
