1e7bbca0187b9e7fa09704b45297aa694f17f6af
Mior Muhammad Zaki <crynobone@gmail.com>
2020-02-24 13:20:21 +0800

    Prepare 5.0.0 release.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Facade.php
+++ src/Facade.php
@@ -2,6 +2,17 @@
 
 namespace Orchestra\Imagine;
 
+/**
+ * @method \Imagine\Image\ImageInterface create(\Imagine\Image\BoxInterface $size, \Imagine\Image\Palette\Color\ColorInterface $color = null)
+ * @method \Imagine\Image\ImageInterface open(string|\Imagine\File\LoaderInterface|mixed $path)
+ * @method \Imagine\Image\ImageInterface load(string $string)
+ * @method \Imagine\Image\ImageInterface read(resource $resource)
+ * @method \Imagine\Image\ImageInterface font(string $file, int $size, \Imagine\Image\Palette\Color\ColorInterface $color)
+ * @method \Imagine\Image\ImageInterface setMetadataReader(\Imagine\Image\Metadata\MetadataReaderInterface $metadataReader)
+ * @method \Imagine\Image\Metadata\MetadataReaderInterface getMetadataReader()
+ *
+ * @see \Orchestra\Imagine\ImagineManager
+ */
 class Facade extends \Illuminate\Support\Facades\Facade
 {
     /**
